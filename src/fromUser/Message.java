5e8a7de3dea8d0406f5d73b91a3705098cdfbf68
Nursultan <nurik.2004703@gmail.com>
2023-12-27 01:56:03 +0600

    рун

--- src/fromUser/Message.java
+++ src/fromUser/Message.java
@@ -4,64 +4,115 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The Message class represents a communication message within the educational institution management system.
+ * It contains information about the message history, ID, date of send, text, author, receiver ID, category,
+ * status, and urgency level.
+
+ */
 public class Message {
-    private List<String> messageHistory; 
-    private static Integer ID = 0; 
+
+    // List to store the message history
+    private List<String> messageHistory;
+
+    // Static ID counter for unique identification
+    private static Integer ID = 0;
+
+    // Date when the message is sent
     private Date DateOfSend;
+
+    // Text content of the message
     private String Text;
+
+    // Author of the message
     private User Author;
-    private int receiverid;
+
+    // Receiver ID of the message
+    private int receiverId;
+
+    // Category of the message (e.g., REQUEST, ANNOUNCEMENT)
     private MessageType Category;
+
+    // Status of the message (e.g., PENDING, APPROVED, REJECTED)
     private StatusType Status;
+
+    // Urgency level of the message (e.g., LOW, MEDIUM, HIGH)
     private UrgencyLevel UrgencyLevel;
 
+    // Initialization block to increment ID
     {
-        ID++; // Increment ID
+        ID++;
     }
 
     // Default constructor
     public Message() {
-        
     }
 
-    // Constructor with parameters
-    public Message(String text, User author, int receiverid, MessageType category,
+    /**
+     * Constructor with parameters to create a new Message object.
+     *
+     * @param text         The text content of the message.
+     * @param author       The author of the message.
+     * @param receiverId   The receiver ID of the message.
+     * @param category     The category of the message.
+     * @param status       The status of the message.
+     * @param urgencyLevel The urgency level of the message.
+     */
+    public Message(String text, User author, int receiverId, MessageType category,
                    StatusType status, UrgencyLevel urgencyLevel) {
         this.DateOfSend = new Date();
         this.Text = text;
         this.Author = author;
-        this.receiverid = receiverid;
+        this.receiverId = receiverId;
         this.Category = category;
         this.Status = status;
         this.UrgencyLevel = urgencyLevel;
     }
 
-    // Getters and setters
+    /**
+     * Gets the unique ID of the message.
+     *
+     * @return The unique ID of the message.
+     */
     public Integer getID() {
         return ID;
     }
 
+    /**
+     * Sets the unique ID of the message.
+     *
+     * @param ID The unique ID to be set.
+     */
     public void setID(Integer ID) {
         Message.ID = ID;
     }
 
-   
+    /**
+     * Gets the receiver ID of the message.
+     *
+     * @return The receiver ID of the message.
+     */
+    public int getReceiverId() {
+        return receiverId;
+    }
 
+    /**
+     * Sets the category of the message.
+     *
+     * @param category The category to be set.
+     */
     public void setCategory(MessageType category) {
         this.Category = category;
     }
 
-    // Method to show messages of high urgency
+    /**
+     * Method to show messages of high urgency from the message history.
+     *
+     * @return List of high urgency messages.
+     */
     public List<String> showHighUrgencyMessages() {
         return messageHistory.stream()
                 .filter(msg -> msg.startsWith("HIGH"))
                 .collect(Collectors.toList());
     }
-
-	public int getReceiverId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-  
 }
--- src/manager/News.java
+++ src/manager/News.java
@@ -16,60 +16,85 @@ import research.Researcher;
         }
     }
 
+    /**
+     * The News class represents a news item in the educational institution management system.
+     * It includes information such as headline, topic, comments, publication date, and category.
+  
+     */
     public class News {
-    	private int id;
+
+        // Unique identifier for the news item
+        private int id;
+
+        // The headline of the news
         private String headline;
+
+        // The topic of the news
         private String topic;
+
+        // List of comments associated with the news
         private List<String> comments;
+
+        // The publication date of the news
         private Date publicationDate;
+
+        // The category of the news (e.g., RESEARCH, STUDY)
         private Category category;
 
+        /**
+         * Constructs a new News object with the specified ID, headline, topic, comments, publication date, and category.
+         *
+         * @param id              The unique identifier for the news item.
+         * @param headline        The headline of the news.
+         * @param topic           The topic of the news.
+         * @param comments        The list of comments associated with the news.
+         * @param publicationDate The publication date of the news.
+         * @param category        The category of the news.
+         */
         public News(int id, String headline, String topic, List<String> comments, Date publicationDate, Category category) {
-        	this.id = id;
+            this.id = id;
             this.headline = headline;
             this.topic = topic;
             this.comments = comments;
             this.publicationDate = publicationDate;
             this.category = category;
-    }
-        
-        
+        }
+
+        /**
+         * Gets the unique identifier for the news item.
+         *
+         * @return The ID of the news item.
+         */
         public int getId() {
             return id;
         }
-    
-    public static void viewNews(Vector<News> newsList) {
-        
-        Stream<News> researchNewsStream = newsList.stream()
-                                                  .filter(news -> news.category == Category.RESEARCH);
-        Stream<News> otherNewsStream = newsList.stream()
-                                               .filter(news -> news.category != Category.RESEARCH);
 
-        List<News> sortedNews = Stream.concat(researchNewsStream, otherNewsStream)
-                                      .collect(Collectors.toList());
+        /**
+         * Views news items based on the provided news list.
+         *
+         * @param newsList The list of news items to view.
+         */
+        public static void viewNews(Vector<News> newsList) {
+            Stream<News> researchNewsStream = newsList.stream()
+                                                      .filter(news -> news.category == Category.RESEARCH);
+            Stream<News> otherNewsStream = newsList.stream()
+                                                   .filter(news -> news.category != Category.RESEARCH);
 
-        
-        for (News news : sortedNews) {
-            System.out.println(news.headline + " - " + news.topic + " [" + news.category + "]");
+            List<News> sortedNews = Stream.concat(researchNewsStream, otherNewsStream)
+                                          .collect(Collectors.toList());
+
+            for (News news : sortedNews) {
+                System.out.println(news.headline + " - " + news.topic + " [" + news.category + "]");
+            }
+        }
+
+        /**
+         * Adds a comment to the list of comments associated with the news.
+         *
+         * @param comment The comment to add.
+         */
+        public void addComment(String comment) {
+            comments.add(comment);
         }
     }
 
-    
-    
-    public void addComment(String comment) {
-    	comments.add(comment);
-    }
-    
-   
-    
-    
-    
-    
-    
-                      
-    
-    
-    
-    
-    
-}
