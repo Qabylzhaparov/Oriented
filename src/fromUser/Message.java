38548358f28a13cfbe6dde4b9a1de27381e38099
Yerkosha <118683459+Yerkosha@users.noreply.github.com>
2023-12-26 21:17:55 +0600

    Update Message.java
    Можете исправлять, у меня все

--- src/fromUser/Message.java
+++ src/fromUser/Message.java
@@ -1,132 +1,62 @@
 package fromUser;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import teacher.Teacher;
-
-public class Message{
-	private List<String> messageHistory;     /// eto dolzho byt' v DB
-	private Integer ID;
+public class Message {
+    private List<String> messageHistory; 
+    private static Integer ID = 0; 
     private Date DateOfSend;
     private String Text;
     private User Author;
     private int receiverid;
     private MessageType Category;
     private StatusType Status;
-    private UrgencyLevel UrgencyLevel;     /// kishi arip
+    private UrgencyLevel UrgencyLevel;
 
     {
-    	ID++;                            /// initil. block           
+        ID++; // Increment ID
     }
-    
+
+    // Default constructor
     public Message() {
-    	
+        
     }
-    
-    /// constr with text, status, urgencyLevel(kak ya pokazal)
-    
+
+    // Constructor with parameters
     public Message(String text, User author, int receiverid, MessageType category,
-    		UrgencyLevel urgencyLevel) {
-		super();
-		DateOfSend = new Date();
-		Text = text;
-		Author = author;
-		receiverid = receiverid;
-		Category = category;
-		Status = status;
-		UrgencyLevel = urgencyLevel;
-	}
-
-
-
-	public boolean addMessage(Message m) {
-    	messageHistory.addAll(messageHistory);         /// v DB dolzhen hranit'
-	    return true;                                   
-
-    	
+                   StatusType status, UrgencyLevel urgencyLevel) {
+        this.DateOfSend = new Date();
+        this.Text = text;
+        this.Author = author;
+        this.receiverid = receiverid;
+        this.Category = category;
+        this.Status = status;
+        this.UrgencyLevel = urgencyLevel;
     }
 
-	public String getID() {
-        return this.ID;
+    // Getters and setters
+    public Integer getID() {
+        return ID;
     }
 
-    public void setID(String ID) {
-        this.ID = ID;
+    public void setID(Integer ID) {
+        Message.ID = ID;
     }
 
-    
-    
-    public int getReceiverId() {
-		return receiverid;
-	}
+   
 
-	public void setReceiverId(int receiver) {
-		receiverid = receiver;
-	}
-
-	public Date getDateOfSend() {
-        return this.DateOfSend;
+    public void setCategory(MessageType category) {
+        this.Category = category;
     }
 
-    public void setDateOfSend(Date DateOfSend) {
-        this.DateOfSend = DateOfSend;
-    }
-
-    public String getText() {
-        return this.Text;
-    }
-
-    public void setText(String Text) {
-        this.Text = Text;
-    }
-
-    public User getAuthor() {
-        return this.Author;
-    }
-
-    public void setAuthor(User Author) {
-        this.Author = Author;
-    }
-
-    public String getCategory() {
-        return this.Category;
-    }
-
-    public void setCategory(String Category) {
-        this.Category = Category;
-    }
-
-    public StatusType getStatus() {
-        return this.Status;
-    }
-
-    public void setStatus(StatusType Status) {
-        this.Status = Status;
-    }
-
-    public UrgencyLevel getUrgencyLevel() {
-        return this.UrgencyLevel;
-    }
-
-    public void setUrgencyLevel(UrgencyLevel UrgencyLevel) {
-        this.UrgencyLevel = UrgencyLevel;
-    }
-
-    public double calculateRating() {
-        return 0.0;
-    }
-
-    public List<String> showMessage() {
-        List<String> result = Arrays.stream(UrgencyLevel.values())
-                .filter(urgencyLevel -> urgencyLevel.name().startsWith("HIGH"))
-                .map(Enum::name)
+    // Method to show messages of high urgency
+    public List<String> showHighUrgencyMessages() {
+        return messageHistory.stream()
+                .filter(msg -> msg.startsWith("HIGH"))
                 .collect(Collectors.toList());
-        return result;
     }
-    
 
-
+  
 }
